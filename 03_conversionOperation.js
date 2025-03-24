e69955248ee91c7372820c986976e05e0c15e920
PrachiiAgarwal <pagarwal2611@gmail.com>
2025-03-24 19:01:59 +0000

    Conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 33
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)  //converts into number
+console.log(typeof valueInNumber);
+
+
+//"33" = 33 can easily be converted
+//"33abc"= NaN cannot be converted
+// true = 1; false =0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 = true; 0=false
+//"" = false
+//"hitesh" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
