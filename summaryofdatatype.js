91d0b48cb8a4c04cbce54f00138a6f82dd943ff2
PrachiiAgarwal <pagarwal2611@gmail.com>
2025-04-22 04:02:26 +0000

    summary of datatypes

---
+++ summaryofdatatype.js
@@ -0,0 +1,39 @@
+// JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime. Unlike languages such as Typescript or Java, you don't need to declare the data type of a variable explicitly.
+
+
+// there 2 types of datatypes
+// 1. Primitive and non Primitive datatype based on the memory it is stored and accessed from
+// PRIMITIVE DATATYPE: String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+// Reference type (non primitive): Array, Objects, Functions------return type is function 
+// declaration of symbol
+ //const id = Symbol('123');  // has different return type as well
+ // to represent bigInt just put a n at the end of the number
+ const bigInt = 1234567890n;
+
+ // presentation of reference datatype
+// return type of null is object
+ //const alphabet=("a","b,"c);
+ {// object
+   //let name ="Prahi";
+ }
+
+//const myfunction = function() {
+  //  console.log("Hello World");
+//}
+
+//primitive data type 
+/*==================
+String : type of string
+Number : type of number 
+BigInt : type of Bigint
+Boolean : type of boolean
+Null : type of object *
+Undefined : type of undefined 
+Symbol : type of Symbol
+
+reference data type 
+==================
+array , object , function 
+only fn type of function and other object */
