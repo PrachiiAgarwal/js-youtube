11233b0d0722573073a9889282f3e602261126d1
PrachiiAgarwal <pagarwal2611@gmail.com>
2025-07-15 13:08:31 +0000

    deconstruction and basics about apis

---
+++ Objects&Api.js
@@ -0,0 +1,13 @@
+//destructuring of objects
+const course = {
+    coursename: "js is hindi",
+    price: "199",
+    courseInstructor: "Prachi"
+}
+
+const {courseInstructor: instructor} = course // syntax of deconstruction
+console.log(instructor); // prachi
+
+
+// APIs
+// APIs are used to interact with the backend and fetch datas from the backend
