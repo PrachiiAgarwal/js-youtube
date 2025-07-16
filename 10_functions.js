5c6d44850ff94ba4c34c20d74beaa4bfcc69b4d0
PrachiiAgarwal <pagarwal2611@gmail.com>
2025-07-16 14:03:31 +0000

    all about functions

--- 10_functions.js
+++ 10_functions.js
@@ -13,7 +13,7 @@ addTwoNumbers(3,4)// passing the arguments
 
 function addTwoNumbers(number1, Number2){ // function declaration with parameters
    let result = number1 + number2 // to execute
-   return result
+   return result // after return statement the function does not perform anything
 }
 console.log("Result", result)
 
@@ -25,3 +25,27 @@ function loginUserMessage(username){
 }}
 
 loginUserMessage("Prachi")
+
+
+function calculateCartPrice(...num1){ // this helps in putting a number of values in an array
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,600)) // now num1 will accept all the values this is called read operator
+// if we write function calcukateCartPrice(val1,val2,...num1) // now here what will happen is val1 and val2 will take the first two vlues and any value after that will be accepted by num1
+
+const user = {
+    username: "prachi"
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log('Username is $(anyObject.user) and price is $(anyObject.user)')
+}
+handleObject(user)
+
+const myNewArray = [200, 400, 500, 1000]
+
+function returnSecondValue(getArray){
+    return getArray[1] // it returns the value at index number 1
+}
